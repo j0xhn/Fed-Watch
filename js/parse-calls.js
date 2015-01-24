e520e8aa51efb38ba4519c1c743fe1c5ca87cb52
John D. Storey <johndangerstorey@gmail.com>
2015-01-24 16:47:13 -0700

    Adds rainbow in top nav bar

--- js/parse-calls.js
+++ js/parse-calls.js
@@ -2,14 +2,25 @@ $(document).ready(function(){
 	$('#example').DataTable();
 });
 
+var currentUser = Parse.User.current();
+if (currentUser) {
+    console.log(currentUser)
+} else {
+    // show the signup or login page
+    alert('not logged in')
+}
+
 
 
 var fbLogin = function(){
+	debugger;
 	Parse.FacebookUtils.logIn(null, {
 		success: function(user) {
 			if (!user.existed()) {
+				debugger;
 				alert("User signed up and logged in through Facebook!");
 			} else {
+				debugger;
 				alert("User logged in through Facebook!");
 			}
 		},
